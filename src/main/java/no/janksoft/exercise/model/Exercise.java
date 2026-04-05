361fa4701413c74b22b2d6d799b6fe7eb0854143
Njoter <stianovesen@gmail.com>
2026-04-06 01:01:40 +0200

    Added createExercise method in ExerciseController

--- src/main/java/no/janksoft/exercise/model/Exercise.java
+++ src/main/java/no/janksoft/exercise/model/Exercise.java
@@ -2,11 +2,13 @@ package no.janksoft.exercise.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table(name = "exercises")
 public class Exercise {
 
