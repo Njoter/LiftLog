df4270e9eb3e1701f24b2892d26d08301b3742c2
Njoter <stianovesen@gmail.com>
2026-04-06 03:12:29 +0200

    Added validation for exercise request dto

--- src/main/java/no/janksoft/common/exception/GlobalExceptionHandler.java
+++ src/main/java/no/janksoft/common/exception/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import no.janksoft.common.http.ErrorResponse;
 import no.janksoft.exercise.exception.DuplicateExerciseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +22,24 @@ public class GlobalExceptionHandler {
                 "DUPLICATE_EXERCISE",
                 e.getMessage(),
                 HttpStatus.CONFLICT.value()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ) {
+        Map<String, String> errors = new HashMap<>();
+
+        String message = e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        return errorResponse(
+                "INVALID_REQUEST_FIELDS",
+                message,
+                HttpStatus.BAD_REQUEST.value()
         );
     }
 
--- src/main/java/no/janksoft/exercise/dto/CreateExerciseRequest.java
+++ src/main/java/no/janksoft/exercise/dto/CreateExerciseRequest.java
@@ -1,8 +1,21 @@
 package no.janksoft.exercise.dto;
 
+import jakarta.validation.constraints.*;
+
 public record CreateExerciseRequest(
+        @NotBlank(message = "Exercise name is required")
+        @Size(max = 50, message = "Exercise name must be less than 50 characters")
         String name,
+
+        @PositiveOrZero(message = "Weight must be 0 or greater")
+        @Digits(integer = 4, fraction = 2, message = "Weight may have up to 4 digits and 2 decimal places")
         double weightKg,
+
+        @Positive(message = "At least 1 rep required")
+        @Max(value = 999, message = "Reps cannot exceed 999")
         int reps,
+
+        @Positive(message = "At least 1 set required")
+        @Max(value = 999, message = "Sets cannot exceed 999")
         int sets
 ) {}
