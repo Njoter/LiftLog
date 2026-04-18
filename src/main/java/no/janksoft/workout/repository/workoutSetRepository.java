82224ef20f5bc631cb66e08d22c930f71800965a
Njoter <stianovesen@gmail.com>
2026-04-18 03:16:11 +0200

    Added endpoint for deleting workout set

--- src/main/java/no/janksoft/workout/repository/workoutSetRepository.java
+++ src/main/java/no/janksoft/workout/repository/workoutSetRepository.java
@@ -3,5 +3,5 @@ package no.janksoft.workout.repository;
 import no.janksoft.workout.dto.WorkoutSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WorkoutRepository extends JpaRepository<WorkoutSet, Long> {
+public interface workoutSetRepository extends JpaRepository<WorkoutSet, Long> {
 }
